f7fbc8aad2f20c9b29810028500a9a5eec920023
TheAltintas <Altintas12@outlook.dk>
2024-09-10 10:46:30 +0200

    Booking side mere flot

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -4,9 +4,11 @@ namespace Surfs_Up.Models
 {
 
     public class Booking {
-        public DateTime Date {get; set;}
+        public DateTime StartDate {get; set;}
+        public DateTime EndDate { get; set;}
         public Customer Customer { get; set; }
         public CatalogItem CatalogItem { get; set; }
+        public string Remark { get; set; }
     }
 
 }
