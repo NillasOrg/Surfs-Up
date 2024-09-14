d0d9d6b28b7e2a78fea82a16c5645c9be627a834
Nicklas Kramer <kramernicklas@gmail.com>
2024-09-14 18:00:30 +0200

    Kurv

---
+++ Repository/ShoppingCart.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Surfs_Up.Models;
+
+namespace Surfs_Up.Repository
+{
+    class ShoppingCart
+    {
+        private readonly List<CatalogItem> items;
+        private static ShoppingCart _instance;
+        // Singleton :)
+        public static ShoppingCart GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new ShoppingCart();
+            }
+            return _instance;
+        }
+
+        private ShoppingCart() {
+            items = [];
+        }
+
+        public void AddToCart(CatalogItem catalogItem) {
+            items.Add(catalogItem);
+        }
+
+        public void RemoveFromCart(CatalogItem catalogItem) {
+            items.Remove(catalogItem);
+        }
+        
+        public List<CatalogItem> ShowCart() {
+            return items;
+        }
+    }
+    
+}
