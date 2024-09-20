8ae56f9352b6be867d69b3b6ea1312d50f92dd2b
TheAltintas <Altintas12@outlook.dk>
2024-09-20 12:03:04 +0200

    Noget af log ind siden indtil 24. minut

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Surfs_Up.Models;
+
+namespace Surfs_Up.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registratrion()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Registratrion(RegistrationVeiwModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Customer customer = new Customer();
+                customer.Email = model.Email;
+                customer.Name = model.Name;
+                customer.UserName = model.UserName;
+                customer.Password = model.Password;
+
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+
+                ModelState.Clear();
+                ViewBag.Message = $"{customer.Name} {customer.Email} {customer.Password} er oprettet";
+
+                return View();
+            }
+            return View(model);
+        }
+    }
+}
--- Models/RegistrationVeiwModel.cs
+++ Models/RegistrationVeiwModel.cs
@@ -1,18 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Surfs_Up.Models
 {
-    [Index(nameof(Email), IsUnique = true)]
-    [Index(nameof(UserName), IsUnique = true)]
-    public class Customer
+    public class RegistrationVeiwModel
     {
-        public int CustomerId { get; set; }
         [Required(ErrorMessage = "Indtast gyldigt navn")]
-        [MaxLength(50, ErrorMessage ="Navn må maks være 50 tegn")]
+        [MaxLength(50, ErrorMessage = "Navn må maks være 50 tegn")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Indtats venligst et gyldigt Email")]
         [MaxLength(50, ErrorMessage = "Email må maks være 50 tegn")]
+        [EmailAddress]
 
         public string Email { get; set; }
         [Required(ErrorMessage = "Indtast gyldig Brugernavn")]
@@ -22,9 +19,12 @@ namespace Surfs_Up.Models
         [Required(ErrorMessage = "Indtast korrekt adgangskode")]
         [MaxLength(25, ErrorMessage = "Adgangskode må maks være 25 tegn")]
         [MinLength(8, ErrorMessage = "Adgangskode skal være minimum 8 tegn")]
+        [DataType(DataType.Password)]
 
         public string Password { get; set; }
+        [Compare("Password", ErrorMessage = "Bekærft venligt adgangskode")]
+        [DataType(DataType.Password)]
 
-
+        public string ConfirmPassword { get; set; }
     }
 }
