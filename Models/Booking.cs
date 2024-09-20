053c79afa05a6f9deb86df4ae44e4dca13607f8e
Milad Fahham <miladfahham@gmail.com>
2024-09-20 09:49:28 +0200

    Database med fejl-beskeder

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Surfs_Up.Models
 {
 
     public class Booking {
         public int BookingId { get; set; }
-        public DateTime StartDate {get; set;}
-        public DateTime EndDate { get; set;}
+        [Required(ErrorMessage = "Did you know Rasputin had a 13 inch cock")]
+        public DateTime? StartDate {get; set;}
+        public DateTime? EndDate { get; set;}
+        [Required(ErrorMessage = "No Custard")]
         public Customer Customer { get; set; }
         public List<CatalogItem>? BookingItems { get; set; }
         public string Remark { get; set; }
