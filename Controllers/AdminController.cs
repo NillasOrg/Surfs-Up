b35fee2b32aa7bcc9935bc97c10925a43c220f3f
Nicklas Kramer <kramernicklas@gmail.com>
2024-09-25 10:40:16 +0200

    Admin side 50%

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Surfs_Up.Models;
+
+namespace Surfs_Up.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly AppDbContext _dbContext;
+    
+    public AdminController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public async Task<IActionResult> Index()
+    {
+        BookingViewModel viewModel = new BookingViewModel();
+        viewModel.Bookings = await _dbContext.Bookings.ToListAsync();
+        viewModel.CatalogItems = await _dbContext.CatalogItems.ToListAsync();
+        viewModel.Customers = await _dbContext.Customers.ToListAsync();
+        return View(viewModel);
+    }
+}
