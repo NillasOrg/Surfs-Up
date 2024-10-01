76801d779ddc86bbc3da5d54225daab0ae95c7bd
Nicklas Kramer <kramernicklas@gmail.com>
2024-10-02 01:16:31 +0200

    LOGIN FRA API VIRKER ENDELIG FUCK MIT LIV

--- Data/ApiContext.cs
+++ Data/ApiContext.cs
@@ -5,18 +5,18 @@ namespace Surfs_Up.Data;
 
 public static class ApiContext
 {
-    // HttpClient er det vi skal bruge til at kalde på vores API
     public static HttpClient _apiClient;
 
     public static void Initialize()
     {
-        var handler = new HttpClientHandler
-        {
-            UseCookies = true, // Enables cookie handling
-            CookieContainer = new CookieContainer() // Stores cookies
-        };
+        var handler = new HttpClientHandler();
         _apiClient = new HttpClient(handler);
-        // Angiver adressen på vores API
+
         _apiClient.BaseAddress = new Uri("https://localhost:5005");
+    }
+
+    public static void SetToken(string token)
+    {
+        _apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
     }
 }
---
+++ Migrations/20241001223358_lort.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Surfs_Up.Migrations
+{
+    /// <inheritdoc />
+    public partial class lort : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
